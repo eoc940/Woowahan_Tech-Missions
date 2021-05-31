ecba61591fae4f37623b8ab74df0a22af49f1375
eoc940 <hgk0000@naver.com>
2021-06-01 00:46:35 +0900

    test commit

--- java-baseball-precourse/src/main/java/baseball/Game.java
+++ java-baseball-precourse/src/main/java/baseball/Game.java
@@ -3,7 +3,7 @@ package baseball;
 import java.util.List;
 
 public class Game {
-
+    //test
     //스트라이크 횟수를 반환하는 메서드
     public int strike(List<Integer> myNum, List<Integer> randNum) {
         int strike = 0;
